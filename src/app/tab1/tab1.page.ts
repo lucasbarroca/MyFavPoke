14cdd7b27bd9ec09e179ecd290bbf486cc407834
Lucas Barroca <lucas.lbarroca@gmail.com>
2022-10-12 00:38:35 -0300

    refactor pokemons list page

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,6 +3,7 @@ import { IonInfiniteScroll } from '@ionic/angular';
 import { FavoriteListService } from '../services/favorite-list.service';
 import { PokeApiService } from '../services/poke-api.service';
 import { PokemonListingService } from '../services/pokemon-listing.service';
+import { PokemonListItem } from '../types/pokemon-list-item';
 
 @Component({
   selector: 'app-tab1',
@@ -18,7 +19,7 @@ export class Tab1Page implements OnInit {
     private favList: FavoriteListService
   ) {}
 
-  pokemons = this.pokeList.items;
+  pokemons: PokemonListItem[] = [];
   fakePokemons = [];
   itemsPerPage = 12;
   currentPage = 1;
@@ -28,12 +29,24 @@ export class Tab1Page implements OnInit {
     for (let i = 0; i < this.itemsPerPage; i++) {
       this.fakePokemons.push(i + 1);
     }
+  }
 
+  ionViewWillEnter() {
     console.log('Loading pokemons...');
+    this.pokemons = [];
+    this.currentPage = 1;
     this.loadPokemons();
   }
 
   loadPokemons() {
+    let maxPages = Math.ceil(
+      this.pokeApi.getPokemonsCount() / this.itemsPerPage
+    );
+
+    if (this.currentPage > maxPages) {
+      return;
+    }
+
     this.pokeApi
       .getPokemonsList(
         this.itemsPerPage,
@@ -42,9 +55,15 @@ export class Tab1Page implements OnInit {
       .subscribe({
         next: (data) => {
           let nextPokes = data.results.map((p) => p.name);
-          this.pokeList.addPokemons(nextPokes).then(() => {
+          this.pokeList.addPokemons(nextPokes, this.pokemons).then(() => {
             this.infiniteScroll.complete();
-            this.infiniteScroll.disabled = false;
+
+            if (this.currentPage >= maxPages) {
+              this.infiniteScroll.disabled = true;
+            } else {
+              this.infiniteScroll.disabled = false;
+            }
+
             console.log('Pokemons loaded');
           });
         },
@@ -52,24 +71,12 @@ export class Tab1Page implements OnInit {
   }
 
   loadMore(event) {
-    let maxPages = Math.ceil(
-      this.pokeApi.getPokemonsCount() / this.itemsPerPage
-    );
-
-    if (this.currentPage >= maxPages) {
-      return;
-    }
-
     this.currentPage++;
     this.loadPokemons();
-
-    if (this.currentPage >= maxPages) {
-      event.target.disabled = true;
-    }
   }
 
   toggleFavorite(index: number) {
-    let pokemon = this.pokeList.items[index];
+    let pokemon = this.pokemons[index];
 
     if (this.favList.isFavorite(pokemon.id)) {
       this.favList.removeFavorite(pokemon.id);
