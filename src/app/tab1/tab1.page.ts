091467e81cac6d6b109ecf386132593a54b90c30
Lucas Barroca <lucas.lbarroca@gmail.com>
2022-10-12 18:21:34 -0300

    fix toggle searchbar button bug

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -116,7 +116,9 @@ export class Tab1Page implements OnInit {
 
   searchBlur(event) {
     if (!event.target.value) {
-      this.showSearch = false;
+      setTimeout(() => {
+        this.showSearch = false;
+      }, 500);
     }
   }
 
