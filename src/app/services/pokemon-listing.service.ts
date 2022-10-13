82f74ce0f6a1e7b79026c32bf173ff049b4cf457
Lucas Barroca <lucas.lbarroca@gmail.com>
2022-10-13 12:54:01 -0300

    add default pokemon image

--- src/app/services/pokemon-listing.service.ts
+++ src/app/services/pokemon-listing.service.ts
@@ -80,7 +80,8 @@ export class PokemonListingService {
       pokemon.sprites.other?.dream_world?.front_default ||
       pokemon.sprites.other?.home?.front_default ||
       pokemon.sprites.other?.['official-artwork']?.front_default ||
-      pokemon.sprites.front_default
+      pokemon.sprites.front_default ||
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/0.png'
     );
   }
 }
